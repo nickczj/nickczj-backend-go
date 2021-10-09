ab385362eefaf0a055ca693bd6eb42df70eb5de8
nczj <nick.chow.zj@gmail.com>
2021-10-09 13:29:30 +0800

    .

--- resources/helpful-commands.sh
+++ resources/helpful-commands.sh
@@ -1,1 +1,11 @@
+# Handy docker stuff
+docker container prune
+docker volume prune
+docker stop container_name
+
+# Show Nginx config
 docker exec nick_nginx-proxy_1 cat /etc/nginx/conf.d/default.conf
+
+# ARCHIVE
+docker exec hosting_plausible_db_1 psql -U postgres -d plausible_db -c "show tables;"
+docker run -it --net hosting_default --rm --link plausible_events_db_1:clickhouse-server yandex/clickhouse-client --host plausible_plausible_events_db_1  --query "CREATE DATABASE IF NOT EXISTS plausible_events_db"
--- resources/run-all.sh
+++ resources/run-all.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml up -d --build --force-recreate
